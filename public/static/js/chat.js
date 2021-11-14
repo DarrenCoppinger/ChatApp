bdf6f8305372a79a262c4de999e4f064505b4f05
DarrenCoppinger <darrencoppinger@gmail.com>
2021-11-14 16:06:38 +0000

    add comments to chat.js file

--- public/static/js/chat.js
+++ public/static/js/chat.js
@@ -1,45 +1,37 @@
 var comment = [];
-var handleID = document.getElementById('exampleFormControlInput1')
-var textID = document.getElementById('exampleFormControlTextarea1');
+var handleID = document.getElementById('handleInput')
+var textID = document.getElementById('textareaInput');
 var form = document.getElementById('form');
 var postdate;
 var timeDiff;
 var timeStamp;
 var totalLikes =0;
-var sHTML = "No Comments";
-
+var sHTML = "<div class=\"card commment-card bg-light\"> \n" +
+                "<div class=\"card-body\"><span class=\"card-text\"> No Comments </span></div>" +
+            "</div>";
 var commentSectionID = document.getElementById('commentSection');
 commentSectionID.innerHTML = sHTML;
-var timetest =  (Date.now() - 60000);  //1 mins
-var timetest0 = (Date.now() - 300000);  //5 mins
-var timetest1 = (Date.now() - 3600000);  //1 hour
-var timetest2 = (Date.now() - 18000000);  //5 hours
-var timetest3 = (Date.now() - 86400000); //yesterday
-var timetest4 = (Date.now() - 172800000);  //>yesterday
-comment.push({handle:"Sunshine", text:"Test-4", postdate:timetest4});
-comment.push({handle:"Sunshine", text:"Test-3", postdate:timetest3});
-comment.push({handle:"Sunshine", text:"Test-2", postdate:timetest2});
-comment.push({handle:"Sunshine", text:"Test-1", postdate:timetest1});
-comment.push({handle:"Sunshine", text:"Test0", postdate:timetest0});
-comment.push({handle:"Sunshine", text:"Test", postdate:timetest});
 
+
+// function triggered clicking post comment button
+// Stores Handle, comment and posting time/date to object
 function addComment(event){
+    //prevent form resting page
     event.preventDefault();
     postdate = new Date();
-
     comment.push({handle:handleID.value, text:textID.value, postdate:postdate});
     displayComments();
+    //clear handle and comment input on form for next comment to be entered
     form.reset();
-    console.log("timeStamp = " + timeStamp);
 }
 
 function displayComments(){
+    //clear sHTML
     sHTML = "";
-    console.log("length= " + comment.length);
-
+    // going from newest to oldest calculate timestamp for each comment
+    // Display previous comments using for loop
     for(var i=(comment.length-1); i>=0; i--){
         timeDiff =((Date.now() - comment[i].postdate)/1000);
-        console.log("timeDiff " + timeDiff);
         if(timeDiff<2) {
             timeStamp = "Posted about a second ago";
         }else if(2<=timeDiff && timeDiff<60){
@@ -55,27 +47,35 @@ function displayComments(){
         }else if(86400<=timeDiff && timeDiff<172800){
             timeStamp = "Posted yesterday";
         } else {
+            // if comment posted before yesterday display formatted date
             var date = new Date(comment[i].postdate);
             var dd = String(date.getDate()).padStart(2, '0');
             var mm = String(date.getMonth() + 1).padStart(2, '0'); //add 1 as January is 0
             var yyyy = date.getFullYear();
-            console.log("dd mm yyyy =" + dd + "/" + mm + "/" +  yyyy);
             timeStamp = "Posted on "  + dd + "/" + mm + "/" +  yyyy;
         }
 
-        sHTML +="<div class=\"card bg-light\" style=\"width: 18rem;\"> \n" +
-            "<div class=\"card-body\"> \n " +
-            "<h5 className=\"card-title\">@" + comment[i].handle + "</h5> \n" +
-            "<small className=\"card-subtitle mb-2 text-muted\">"  +  timeStamp +  "</small> \n" +
-            "<p class=\"card-text mt-3\">" + comment[i].text + "</p> \n" +
-            "<p><button class=\"btn btn-primary\" onclick=\'addLike();\'>Like</button>" + "</p> \n " +
-            "</div>" +
-            "</div>" +
-            "<BR>";
+        sHTML +=
+            "<div class=\"card commment-card bg-light\"> \n" +
+                "<div class=\"card-body\"> \n " +
+                    "<h6 className=\"card-title\">@" + comment[i].handle + " </h6>" +
+                    "<span class=\"card-text\">" + comment[i].text + "</span> \n" +
+                "</div>" +
+                "<div class=\"card-footer align-middle\">  \n" +
+                    "<div class=\"row g-0 pl-3 ml-1 \">" +
+                        "<div class=\"col-8 align-center text-muted\">" +
+                            "<span className=\"card-subtitle \">"  +  timeStamp +  "</span>" +
+                        "</div>" +
+                        "<div class=\"col-4 float-end text-end\">" +
+                            "<button class=\"btn btn-primary btn-sm \" onclick=\'addLike();\'><i class=\"far fa-thumbs-up\"></i> Like</button>" +
+                        "</div>" +
+                    "</div>" +
+                "</div>" +
+            "</div>";
     }
     commentSectionID.innerHTML = sHTML;
 }
-
+// Each time like button pressed add one to totalLikes and display with alert
 function addLike(){
     totalLikes++;
     alert(" Total likes = "+ totalLikes)
